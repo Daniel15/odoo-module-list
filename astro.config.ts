89c5e814f5e83c9741ad2b39cb93d63318c2646c
Daniel Lo Nigro <git@d.sb>
2026-02-10 19:58:58 -0800

    Pre-compress content

--- astro.config.ts
+++ astro.config.ts
@@ -1,17 +1,31 @@
 import {defineConfig} from 'astro/config';
 
+import zlib from 'node:zlib';
 import purgecss from 'astro-purgecss';
+import compressor from 'astro-compressor';
 
 export default defineConfig({
   build: {
     assets: '_build',
   },
-  compressHTML: false,
   site: process.env.SITE_URL ?? 'https://odoomodules.com/',
   integrations: [
     purgecss({
       keyframes: true,
       variables: true,
+    }),
+    compressor({
+      brotli: {
+        params: {
+          [zlib.constants.BROTLI_PARAM_QUALITY]:
+            zlib.constants.BROTLI_MAX_QUALITY,
+        },
+      },
+      zstd: {
+        params: {
+          [zlib.constants.ZSTD_c_compressionLevel]: 19,
+        },
+      },
     }),
   ],
   vite: {
@@ -19,7 +33,12 @@ export default defineConfig({
       // https://getbootstrap.com/docs/5.3/getting-started/vite/#configure-vite
       preprocessorOptions: {
         scss: {
-          silenceDeprecations: ['if-function', 'import', 'color-functions', 'global-builtin'],
+          silenceDeprecations: [
+            'if-function',
+            'import',
+            'color-functions',
+            'global-builtin',
+          ],
         },
       },
     },
